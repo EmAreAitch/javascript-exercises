16ee0d9d33df92b752ce8bc596d9d0c78aa9717d
EmAreAitch <rahibhasan95@gmail.com>
2022-11-19 21:30:13 +0530

    Solved

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    x = parseInt(num)
+    if (!(x<0)) {
+	let fibonacciList = [1,1];
+	    for (i = 1; i <=x-2; i++) {
+	        fibonacciList.push(parseInt(fibonacciList.slice(-1))+parseInt(fibonacciList.slice(-2,-1)))
+	    }
+	    return fibonacciList[num-1];
+	
+    }
+    else return "OOPS";
 };
 
 // Do not edit below this line
