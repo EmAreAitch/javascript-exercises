28bd0e3ca094841c19bb3d5ebdd63821986bf887
EmAreAitch <rahibhasan95@gmail.com>
2022-11-20 00:05:49 +0530

    Optimized Code

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,13 +1,16 @@
 const fibonacci = function(num) {
-    x = parseInt(num)
-    if (!(x<0)) {
-	let fibonacciList = [1,1];
-	    for (i = 1; i <=x-2; i++) {
-	        fibonacciList.push(parseInt(fibonacciList.slice(-1))+parseInt(fibonacciList.slice(-2,-1)))
-	    }
-	    return fibonacciList[num-1];
-	
-    }
+	if (num>0) {
+		a = 0;
+		b = 1;
+		for(i = 1; i < num; i++) {
+			c = b;
+			b = a + b;
+			a = c;
+		}
+		return b;
+	}
+
+	else if (num===0) return 0;
     else return "OOPS";
 };
 
